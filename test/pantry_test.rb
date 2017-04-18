02b3642b5c5d6e53a1ab4ded773b100e219b98c5
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-18 09:38:28 -0600

    Initial commit for pantry and updated pantry_test to remove initialization object

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
   def test_object_is_a_pantry
-    p = Pantry.new("Cheese")
+    p = Pantry.new
 
     assert_instance_of Pantry, p
   end
